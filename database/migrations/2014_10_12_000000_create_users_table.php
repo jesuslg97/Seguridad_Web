2dadee0c4e0ee130ac5765100ec7037f0b74f6d0
jesus <jelopezgonzalez@gmail.com>
2022-09-23 17:49:15 +0100

    add fields in table user and migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('surname');
+            $table->string('dni');
+            $table->string('phone');
+            $table->string('country');
+            $table->string('IBAN');
+            $table->string('about');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
