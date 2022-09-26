a94c3f615fbfbb8131e19c28098df35bc538220d
josu <josu_gutierrez@hotmail.com>
2022-09-26 20:42:22 +0200

    Seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+             'id' => 1,
+             'name' => 'Prueba',
+             'surname' => 'Prueba',
+             'dni' => '123456789L',
+             'phone' => '123456789',
+             'country' => 'España',
+             'IBAN' => ' ES91 2100 0418 45 0200051332',
+             'about' => 'Sobre mi',
+             'email' => 'seguridadweb@campusviu.es',
+             'password' => Hash::make('S3gur1d4d?W3b')
+             
+
+         ]);
     }
 }
